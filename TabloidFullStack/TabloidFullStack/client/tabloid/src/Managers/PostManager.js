a360c0a713cf06379d6c3215f70b7b72cbb2f2da
dmaceylewis <danna.macey.lewis@gmail.com>
2024-09-18 13:40:15 -0400

    added post route to application view

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/PostManager.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/Managers/PostManager.js
@@ -1,5 +1,6 @@
 const apiUrl = "https://localhost:5001";
 
+//fetch to get list of Posts from the api
 export const getAllPosts = () => {
     return fetch(`${apiUrl}/api/Post`) 
       .then((res) => res.json())
