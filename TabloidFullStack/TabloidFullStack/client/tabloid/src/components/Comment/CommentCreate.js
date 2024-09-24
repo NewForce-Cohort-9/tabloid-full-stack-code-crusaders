ac3bc8f5a9531151f5533b1c0f1829332a976a72
Rev Huffman <148085981+Revvvolution@users.noreply.github.com>
2024-09-24 18:01:02 -0400

    Added state to capture post title and added Cancel button/ styling for CommentCreate.js module

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Comment/CommentCreate.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Comment/CommentCreate.js
@@ -3,11 +3,13 @@ import { Link, useNavigate } from "react-router-dom";
 import { Button, Form, FormGroup, Input, Label } from "reactstrap";
 import { addComment } from "../../Managers/CommentManager.js";
 import { useParams } from 'react-router-dom';
+import { getPostById } from "../../Managers/PostManager.js";
 
 
 
 export const CommentCreate = () => {
     const postId = useParams();
+    const [postTitle, setPostTitle] = useState("");
     const [userProfileId, setUserProfileId] = useState(null);
     const [subject, setSubject] = useState('');
     const [content, setContent] = useState('');
@@ -19,6 +21,9 @@ export const CommentCreate = () => {
         if (userProfile && userProfile.id) {
             setUserProfileId(userProfile.id);
         }
+        getPostById(postId.id).then((postObj) => {
+            setPostTitle(postObj.title)
+        });
     }, []);
 
     const handleSubmit = async (e) => {
@@ -42,7 +47,10 @@ export const CommentCreate = () => {
         
 
     return (
-        <Form onSubmit={(e) => { handleSubmit(e); }}>
+        <div className="create-container">
+            <h1 style={{textAlign: "center", textWrap:"wrap"}}>New Comment for Post: "{postTitle}"</h1>
+        <Form onSubmit={(e) => { handleSubmit(e); }}
+                style={{fontSize: "22px", fontWeight: "bold", textAlign: "center"}}>
             <FormGroup className="mt-4">
                 <Label for="subject">Subject</Label>
                 <Input
@@ -65,7 +73,11 @@ export const CommentCreate = () => {
                     required
                 />
             </FormGroup>
-            <Button type="submit">Save</Button>
+            <div className="col-md-12 mt-5" style={{display: "flex", justifyContent: "space-evenly"}}>
+            <Button className="col-md-2" type="submit" color="success">Save</Button>
+            <Button className="col-md-2" type="back" color="danger" outline onClick={() => navigate(-1) }>Cancel</Button>
+            </div>
         </Form>
+        </div>
     );
 };
