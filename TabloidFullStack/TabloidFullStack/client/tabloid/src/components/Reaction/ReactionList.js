a331d019196a174fef830e3fa82b39845b81ba91
Chelsea Brooks <cbrooks6212@gmail.com>
2024-09-26 15:30:04 -0400

    new main

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Reaction/ReactionList.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Reaction/ReactionList.js
@@ -1,10 +1,37 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { getAllReactions } from '../../Managers/ReactionManager.js';
+import { Link } from 'react-router-dom';
+import { Button, Col } from 'reactstrap';
 
 export const ReactionList = () => {
     const [reactions, setReactions] = useState([]);
 
-    
+    const getReactions = () => {
+        getAllReactions().then((allReactions) => setReactions(allReactions))
+    };
+
+    useEffect(() => {
+        getAllReactions();
+    }, []);
+
+
   return (
-    <div>ReactionList</div>
+    <div>
+      <h2>Reactions</h2>
+      <div>
+        <Link to="/reaction/add" key="reaction name">
+            <Col>
+                <Button color="info">Add a new Reaction</Button>
+            </Col>
+        </Link>
+        </div>
+        <div>
+        {Reaction.map((reaction) => (
+          <ul>
+            <Reaction key={reaction.id} reaction={reaction}/>
+          </ul>
+        ))}
+      </div>
+    </div>
   )
 }
