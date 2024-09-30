a5f0f495b76aabb324e648846719de805c34181c
dmaceylewis <danna.macey.lewis@gmail.com>
2024-09-30 11:16:35 -0400

    started filter posts by user

--- TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Post/PostList.js
+++ TabloidFullStack/TabloidFullStack/client/tabloid/src/components/Post/PostList.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Post } from "./Post";
 import { Search } from "./Search";
-import { getAllPosts, getPostsByCategory } from "../../Managers/PostManager";
+import { getAllPosts, getPostsByCategory, getPostsByUserId } from "../../Managers/PostManager";
 import { getAllCategories } from "../../Managers/CategoryManager";
 import { Link } from "react-router-dom";
 import { Button, Col, Row } from "reactstrap";
@@ -9,7 +9,7 @@ import { Button, Col, Row } from "reactstrap";
 export const PostList = () => {
   const [posts, setPosts] = useState([]);
   const [search, setSearch] = useState({});
-  const [postCategories, setPostCategories] = useState([]); 
+  const [postCategories, setPostCategories] = useState([]);
 
   const getPosts = () => {
     getAllPosts().then(allPosts => setPosts(allPosts)); 
@@ -24,10 +24,30 @@ export const PostList = () => {
   }, []); 
 
 
+   // Filter Posts by Category
   const postsByCategory = async (id) => {
     if (id > 0)
     {
       const posts = await getPostsByCategory(id);
+      setPosts(posts)
+    }
+    else
+    {
+      getPosts()
+    }
+  }
+
+  // Filter Posts by User
+  const userProfileId = JSON.parse(localStorage.getItem("userProfile")).id;
+
+  useEffect(() => {
+    getPostsByUserId(userProfileId).then((data) => setPosts(data));
+  }, [userProfileId]);
+
+  const postsByUser = async (id) => {
+    if (id > 0)
+    {
+      const posts = await getPostsByUserId(id);
       setPosts(posts)
     }
     else
@@ -53,6 +73,17 @@ export const PostList = () => {
                             {postCategories.map(category => {
                                 return <option value={`${category.id}`} >
                                         {category.name}
+                                      </option>
+                            })}
+              </select>
+            </Col>
+            <Col md={6}>
+              <select name="users" id="filterPostUsers" 
+                  onChange={(event) => {return postsByUser(parseInt(event.target.value))}}>
+                        <option selected>Filter by User:</option>
+                            {posts.map(user => {
+                                return <option value={`${user.id}`} >
+                                        {user.name}
                                       </option>
                             })}
               </select>
